89c1ff53b7c7b929668c943b428b5bdc07132770
KChanho <kch10406@gmail.com>
2022-02-08 17:40:25 +0900

    lat / lon print

--- js/geo.js
+++ js/geo.js
@@ -1,14 +1,15 @@
-const API_KEY = "d7a055de554dc39c576a565aeeda0d43";
-
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const txt = document.querySelector("#weather span");
-    txt.innerText = `latitude: ${lat} / longitude: ${lon}`;
+    
+    const latSpan = document.querySelector("#location span:first-child");
+    const lonSpan = document.querySelector("#location span:last-child");
+    latSpan.innerText = `lat: ${Math.round(lat * 10000) / 10000}`;
+    lonSpan.innerText = `lon: ${Math.round(lon * 10000) / 10000}`;
 }
 
 function onGeoError() {
-    alert("Can't find you. Where are you.");
+    alert("Can't find you. No location for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
